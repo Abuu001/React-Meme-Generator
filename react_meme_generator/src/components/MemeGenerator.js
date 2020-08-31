c33d311375f431799d5d275c1415d54e07163054
Abuu001 <abulugz@gmail.com>
2020-08-31 10:25:24 +0300

    Added header and meme gen

---
+++ react_meme_generator/src/components/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor(){
+        super()
+        this.state={}
+    }
+    render(){
+        return(
+            <div>
+                <h3>meme</h3>
+            </div>
+        )
+    }
+}
+export default MemeGenerator
